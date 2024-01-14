4f7bd687cf97536fa131f31ac315c823f2a00a8e
NectoT <nectot2@gmail.com>
2024-01-14 23:02:44 +0300

    Сигнал об изменении NumberInput посылается после каждого успешного изменения ввода

--- ui/number_edit.gd
+++ ui/number_edit.gd
@@ -4,8 +4,9 @@ signal number_submitted(new_number: int)
 
 @export var number = 0:
 	set(value):
+		if value != number:
+			text = str(value)
 		_number = value
-		text = str(value)
 		_prev_text = text
 	get:
 		return _number
@@ -17,25 +18,15 @@ var _prev_text = ''
 func _ready() -> void:
 	_prev_text = str(number)
 	text_changed.connect(_on_text_changed)
-	
-	focus_exited.connect(_on_focus_exited)
-	text_submitted.connect(_on_text_submitted)
 
 
 func _on_text_changed(new_text: String):
 	if new_text.is_valid_int():
 		_number = text.to_int()
+		number_submitted.emit(_number)
 	elif new_text != '':
 		var caret_pos = caret_column
 		text = _prev_text
 		caret_column = caret_pos - 1  # Но почему нужно вычитать единицу?
 	
 	_prev_text = text
-
-
-func _on_focus_exited():
-	number_submitted.emit(number)
-
-
-func _on_text_submitted(new_text: String):
-	number_submitted.emit(number)
